0ce5b5696df7ea4e45e40052ab67ef51603f8281
dennisrein <dennis.rein@outlook.com>
2022-01-22 02:56:18 +0100

    Initial commit

---
+++ src/main.js
@@ -0,0 +1,33 @@
+const fs = require('fs');
+const { Client, Collection, Intents } = require('discord.js');
+
+const dotenv = require('dotenv');
+
+dotenv.config();
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+
+for (const file of eventFiles) {
+	const event = require(`./events/${file}`);
+	if (event.once) {
+		client.once(event.name, (...args) => event.execute(client, ...args));
+	} else {
+		client.on(event.name, (...args) => event.execute(client, ...args));
+	}
+}
+
+
+client.commands = new Collection();
+const commandFiles = fs.readdirSync('./src/commands').filter(file => file.endsWith('.js'));
+
+for (const file of commandFiles) {
+	const command = require(`./commands/${file}`);
+	// Set a new item in the Collection
+	// With the key as the command name and the value as the exported module
+	client.commands.set(command.data.name, command);
+}
+
+
+client.login(process.env.DISCORD_TOKEN);
