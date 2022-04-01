cc3d17b37dd1a71b51bcb3b488a851bfff630995
dennisrein <dennis.rein@outlook.com>
2022-04-01 14:30:21 +0000

    Update logging

--- src/events/messageUpdate.js
+++ src/events/messageUpdate.js
@@ -12,6 +12,9 @@ module.exports = {
 
         if(newMessage.author.bot) return;
 
+        console.log("New", newMessage);
+        console.log("Old", oldMessage);
+
         writeLogMessage({client: client, type: "messageUpdate", args: oldMessage, newMessage});
 	},
 };
